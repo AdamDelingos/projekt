f596922ca9651fcc9893dad6cc240a85f87d7294
Piotr Jura <yourii@tlen.pl>
2014-06-02 19:33:57 +0200

    Dodanie klasy kwadrat
    działające obliczenie pola kwadratu

---
+++ WindowsFormsApplication1/Kwadrat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    public class Kwadrat
+    {
+        public int poleKwadratu(int a)
+        {
+            if (a < 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return a * a;
+            }
+        }
+    }
+}
